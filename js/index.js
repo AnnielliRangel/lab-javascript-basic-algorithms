4dcbd82cd48c0d691f513143418bedd5156da1a7
Annielli Rangel <anniellirangel@gmail.com>
2022-10-19 23:43:03 -0300

    Lab3 done

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 // Iteration 1: Names and Input
 
-const hacker1="Annielli";
+const hacker1="Bob";
 //console.log(`The driver's name is ${hacker1}`);
-const hacker2="Bob";
+const hacker2="Maria";
 //console.log(`The navigator's name is ${hacker2}`)
 
 
@@ -12,10 +12,10 @@ const hacker2="Bob";
 //console.log(`The name ${hacker2} has ${hacker1.length} characters`)
 
 if (hacker1.length > hacker2.length){
-    console.log(`The driver has the longest name, it has ${hacker1.length} characters`)
+    console.log(`The driver ${hacker1} has the longest name, it has ${hacker1.length} characters`)
 }
 if (hacker1.length < hacker2.length){
-    console.log(`It seems that the navigator has the longest name, it has${hacker1.length} characters`)
+    console.log(`It seems that the navigator ${hacker2} has the longest name, it has ${hacker2.length} characters`)
 }
 if (hacker1.length == hacker2.length){
     console.log(`Wow, you both have equally long names, with ${hacker1.length} characters!`)
@@ -33,7 +33,31 @@ for(let i=hacker1.length-1; i>=0; i--){
     reverse+=hacker1[i]
 
 }
-console.log(reverse)
+console.log(reverse.toUpperCase())
 
+/*
+switch(let comp= hacker1.localeCompare(hacker2)){
+    case(comp>0):
+        console.log(`The driver's name goes first.`)
+        break;
+    case(comp<0):
+        console.log(`Yo, the navigator goes first definitely.`)
+        break;
+    default;
+        console.log(`What?! You both have the same name?`)
+        break;
 
+}*/
+let maior= hacker1.localeCompare(hacker2)
+    console.log(maior)
+
+if(maior<0){
+    console.log(`The driver's name goes first.`)
+}
+if(maior>0){
+    console.log(`Yo, the navigator goes first definitely.`)
+}
+if(maior==0){
+    console.log(`What?! You both have the same name?`)
+}
 
